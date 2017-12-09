fde1d8c94f227cca9496d9fbd1f6fc436bbd2edd
Clayton Massie <claytonmassie@Claytons-MacBook-Pro.local>
2017-12-08 19:26:51 -0600

    uploaded videos to database using paperclip

--- app/models/audio.rb
+++ app/models/audio.rb
@@ -1,4 +1,6 @@
 class Audio < ApplicationRecord
-    has_attached_file :audio_file, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-    validates_attachment_content_type :audio_file, :content_type => /\Aimage\/.*\Z/
+    has_attached_file :audio_file 
+    validates_attachment_content_type :audio_file, :content_type => [ 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio' ]
 end
+
+
