ef230b5150f9a75e63d52dd1e582d5c4716347d4
Clayton Massie <claytonmassie@Claytons-MacBook-Pro.local>
2017-12-14 15:58:23 -0600

    stuff

--- lib/calendar.rb
+++ lib/calendar.rb
@@ -38,7 +38,7 @@ def self.authorize
     puts "Open the following URL in the browser and enter the " +
          "resulting code after authorization"
     puts url
-    code = gets
+    code = "4/y8-4uKx2c6y9XT1lWme0fd_byGLq5isOMx4YJMDfj-Y"
     credentials = authorizer.get_and_store_credentials_from_code(
       user_id: user_id, code: code, base_url: OOB_URI)
   end
